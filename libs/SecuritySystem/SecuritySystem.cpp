d7ada4c56ac9fcfba75790ba8c3e8e1a068b50ee
KPMGE <jesuskevin254@gmail.com>
2021-05-06 20:10:35 -0300

    Adding implementation of a library for security system

---
+++ libs/SecuritySystem/SecuritySystem.cpp
@@ -0,0 +1,33 @@
+// implementation of class security system 
+
+#include "Arduino.h"
+#include "SecuritySystem.h"
+
+#define AMOUNT_NOTES 7 
+#define DELAY_BUZZER 1000 
+const int notes[] = { 524, 588, 660, 699, 785, 881, 989 }; // notes for buzzer
+
+SecuritySystem::SecuritySystem(int ldrPin, int buzzerPin, int maxValueLdr) {
+  pinMode(buzzerPin, OUTPUT);
+
+  _ldrPin = ldrPin;
+  _buzzerPin = buzz;
+  _maxValueLdr = maxValueLdr;
+}
+
+bool SecuritySystem::wasLaserInterrupted() {
+  const int valueLDR = analogRead(LDR_PIN);
+
+  if (valueLDR < maxValueLdr) {
+    return true;
+  }
+  return false;
+}
+
+void SecuritySystem::triggerAlarm() {
+  for (int i = 0; i < AMOUNT_NOTES; i++) {
+    tone(BUZZER_PIN, notes[i], DELAY_BUZZER);
+    delay(DELAY_BUZZER);
+  }
+  delay(DELAY_BUZZER);
+}
