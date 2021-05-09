e49c9238234c5d6a09f62e29005c5de99af324f3
KPMGE <jesuskevin254@gmail.com>
2021-05-09 17:54:37 -0300

    adding garage controller with millis implementation

---
+++ libs/GarageControllerMillis/GarageControllerMillis.cpp
@@ -0,0 +1,38 @@
+#include "Arduino.h"
+#include "GarageControllerMillis.h"
+
+#define START_POSITION 0
+
+GarageControllerMillis::GarageControllerMillis(int servoPin, int servoSpeed, int servoMaxAngle, int ledPin, int mindDistance) {
+  pinMode(servoPin, OUTPUT);
+  pinMode(ledPin, OUTPUT);
+  _servo.attach(servoPin);
+
+  _servoPin = servoPin;
+  _servoSpeed = servoSpeed;
+  _servoMaxAngle = servoMaxAngle;
+  _ledPin = ledPin;
+  _mindDistance = mindDistance;
+}
+
+void GarageControllerMillis::openGarage() {
+  _servo.slowmove(_servoPin, _servoSpeed);
+}
+
+void GarageControllerMillis::closeGarage() {
+  _servo.slowmove(START_POSITION, _servoSpeed);
+}
+
+void GarageControllerMillis::turnOnLed() {
+  digitalWrite(_ledPin, HIGH);
+}
+
+void GarageControllerMillis::turnOffLed() {
+  digitalWrite(_ledPin, LOW);
+}
+
+bool GarageControllerMillis::isCarNear(int distance) {
+  return (distance < _mindDistance);
+}
+
+
