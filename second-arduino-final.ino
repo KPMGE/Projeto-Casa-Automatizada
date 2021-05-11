ab7bd79f14371368e29bd6a7a718a75ea51ef482
KPMGE <jesuskevin254@gmail.com>
2021-05-11 19:08:54 -0300

    Adding code for second arduino

---
+++ second-arduino-final.ino
@@ -0,0 +1,127 @@
+#include <idDHT11.h> 
+#include <SaveEnergySystem.h>
+#include <SecuritySystem.h>
+
+// constants for security system
+#define LDR_PIN_SECURITY A0
+#define MAX_VALUE_LDR 1000
+#define BUZZER_PIN 5
+
+// instance of security system
+SecuritySystem security(LDR_PIN_SECURITY, BUZZER_PIN, MAX_VALUE_LDR);
+
+
+// constants for save energy system
+#define LDR_PIN_ENERGY A1
+#define MAX_LIGHT_LEVEL 200
+#define AMOUNT_LEDS 4
+// house leds 
+int ledsPin[] = { 8, 9, 10, 11 };
+// instance of class
+SaveEnergySystem lights(LDR_PIN_ENERGY, MAX_LIGHT_LEVEL, ledsPin, AMOUNT_LEDS);
+
+
+// constants for DHT sensor
+#define DHT_PIN 2 
+#define DHT_INTERRUPT 11 
+
+// interruption controller
+void dht11_wrapper();    
+// update sensor values
+void loopDHT();         
+
+// instance of class
+idDHT11 dhtSensor(DHT_PIN, DHT_INTERRUPT, dht11_wrapper); 
+float temperatureC, umidity;
+
+
+void setup() {
+	Serial.begin(9600); 
+}
+
+void loop() {
+	// monitoring lights
+  lights.lightsAutomaticManager();
+
+	// checking security system
+  if (security.wasLaserInterrupted()) {
+    security.triggerAlarm();
+  }
+
+  // update data
+	loopDHT(); 
+	// display data
+	Serial.print("Temperatura em Celcius: ");
+	Serial.println(temperatureC);
+	Serial.print("Umidade Relativa: ");
+	Serial.println(umidity);
+}
+
+// interruption callback
+void dht11_wrapper() {
+	dhtSensor.isrCallback();
+}
+
+void loopDHT() {
+	static int tempo_leitura = 1000; // 1 segundo
+	static unsigned long delay_leitura = millis() + tempo_leitura + 1;
+	static bool request = false;
+
+	if ((millis() - delay_leitura) > tempo_leitura) { 
+		if (! request) {
+			 dhtSensor.acquire(); 
+			 request = true;
+		}
+	}
+
+	if (request && ! dhtSensor.acquiring()) {
+		request = false;
+
+		int result = dhtSensor.getStatus();
+
+		switch (result) {
+			case IDDHTLIB_OK: 
+				Serial.println("Leitura OK"); 
+				break;
+			case IDDHTLIB_ERROR_CHECKSUM: 
+				Serial.println("Erro\n\r\tErro Checksum"); 
+				break;
+			case IDDHTLIB_ERROR_ISR_TIMEOUT: 
+				Serial.println("Erro\n\r\tISR Time out"); 
+				break;
+			case IDDHTLIB_ERROR_RESPONSE_TIMEOUT: 
+				Serial.println("Erro\n\r\tResponse time out"); 
+				break;
+			case IDDHTLIB_ERROR_DATA_TIMEOUT: 
+				Serial.println("Erro\n\r\tData time out erro"); 
+				break;
+			case IDDHTLIB_ERROR_ACQUIRING: 
+				Serial.println("Erro\n\r\tAcquiring"); 
+				break;
+			case IDDHTLIB_ERROR_DELTA: 
+				Serial.println("Erro\n\r\tDelta time to small"); 
+				break;
+			case IDDHTLIB_ERROR_NOTSTARTED: 
+				Serial.println("Erro\n\r\tNao iniciado"); 
+				break;
+			default: 
+				Serial.println("Erro Desconhecido"); 
+				break;
+		}
+
+		float valor = 0.00;
+
+		value = dhtSensor.getCelsius();
+		if (! isnan(value)) {      
+			temperatureC = value;
+		}
+
+		value = dhtSensor.getHumidity();
+		if (! isnan(value)) {      
+			umidity = value;
+		}  
+
+		delay_leitura = millis();     
+	}
+}
+
